5240a21ce0a25477cb7fcbd6bb2a061c5c36aea6
Andrei Bernatski <bernatski.a@gmail.com>
2023-02-25 11:55:42 +0300

    feat: add webpack build

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: './momentum/js/index.js',
+    output: {
+        path: `${__dirname}/momentum/js`,
+        filename: 'build.js',
+    },
+    mode: 'production',
+    // mode: 'development',
+}
